dc3c1155a2a49a433c3ef265ec1ab443907f3158
usuario <usuario@10.22.146.196>
2013-09-20 14:03:18 +0100

    1.2

--- src/kata1/Student.java
+++ src/kata1/Student.java
@@ -5,6 +5,23 @@ import java.util.Date;
 
 public class Student {
 
+    
+
     private String name;
     private Date birthDate;
+    
+    public Student(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
 }
